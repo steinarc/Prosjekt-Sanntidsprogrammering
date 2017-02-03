5593b2b078f30136e7acd6a7179bdb763bbdc900
karineav <karineav@stud.ntnu.no>
2017-02-03 12:44:22 +0100

    Add files via upload

---
+++ Network/Orderstruct.py
@@ -0,0 +1,4 @@
+class Order:
+	def __init__(self, floor, direction):
+		self.floor = floor
+		self.direction = direction
---
+++ Network/receive.py
@@ -0,0 +1,6 @@
+from Network import *
+
+#a = UDP_receive_and_confirm('127.0.0.1', 20018, 10)
+UDP_receive('127.0.0.1', 20018, 1)
+
+
