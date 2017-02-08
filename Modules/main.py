1527304a11b0a7b01540b5a85cdceb41a6ea32e2
steinarc <Steinar_T_Chri@hotmail.com>
2017-02-08 20:07:20 +0100

    Add files via upload

--- Modules/main.py
+++ Modules/main.py
@@ -5,27 +5,19 @@ from Lift_Control import *
 def main():
 	lift = Lift(1)
 	driver.elev_init()
-	lift.ip_list = ['10.22.69.248', '127.0.0.1', '127.0.0.1']
+	lift.ip_list = ['129.241.187.157', '129.241.187.151', '127.0.0.1']
+	lift.active_lifts = [0, 0, 0]
 
-	#thread_lift_find_floor = Thread(target = lift_find_floor, args = (lift,)) #maybe it is a shallow copy?? carefull!
-	#thread_listen_buttons = Thread(target = listen_all_buttons, args = (lift,))
-	#thread_lift_find_floor.start()
-	#thread_listen_buttons.start()
-	#while(1):
-	#	execute_order(lift)
+	thread_lift_find_floor = Thread(target = lift_find_floor, args = (lift,)) #maybe it is a shallow copy?? carefull!
+	thread_listen_buttons = Thread(target = listen_all_buttons, args = (lift,))
+	thread_receive_message = Thread(target = receive_message_and_act, args = (lift, 20018))
+	thread_lift_find_floor.start()
+	thread_listen_buttons.start()
+	thread_receive_message.start()	
 
-	message = "1,Command,4 -1"
-	message_type = classify_message(message)
-	#print_order(order)
-	#print message_type
-
-	order = Order(3, -1)
-	a = encode_command_message(lift, order)
-	#print a
-	heisnavn, order_received = decode_command_message(a)
-
-	print_order(order_received)
-	print(heisnavn)
+	while(1):
+		print (lift.active_lifts)
+		time.sleep(1)
 
 	#send_and_spam_until_confirmation('10.22.69.248', 20018, b'Hei sveis')
 
