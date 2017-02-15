c2391558a14415d701992315a660bb428cfc5874
steinarc <Steinar_T_Chri@hotmail.com>
2017-02-15 13:23:29 +0100

    Delete send.py

--- Network/send.py
+++
@@ -1,9 +0,0 @@
-from Network import *
-
-#UDP_send('127.0.0.1', 20018, "Hei sveis")
-
-order = Order(9,-1)
-encoded = encode_order_message(order)
-UDP_send_and_spam_until_confirmation('127.0.0.1', 20018, encoded, 10)
-
-
